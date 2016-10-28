355a976b1b77029d013a0dda5de16d344599b431
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2016-10-28 17:55:18 +0200

    + use lwt for async is_perfect predicate

--- Benchmark/lib/perfect_number.ml
+++ Benchmark/lib/perfect_number.ml
@@ -21,6 +21,31 @@ let is_perfect n =
   (is_perfect 29) false
 *)
 
+(* async predicate to check for a perfect number *)
+let lwt_is_perfect n = Lwt.map is_perfect (Lwt.return n)
+(*$= lwt_is_perfect
+  (lwt_is_perfect 1) Lwt.return_false
+  (lwt_is_perfect 2) Lwt.return_false
+  (lwt_is_perfect 6) Lwt.return_true
+  (lwt_is_perfect 7) Lwt.return_false
+  (lwt_is_perfect 27) Lwt.return_false
+  (lwt_is_perfect 28) Lwt.return_true
+  (lwt_is_perfect 29) Lwt.return_false
+*)
+let lwt_is_perfect_val n =
+  Lwt.map (fun x -> if is_perfect x then Some x else None) (Lwt.return n)
+(*$= lwt_is_perfect_val
+  (lwt_is_perfect_val 1) Lwt.return_none
+  (lwt_is_perfect_val 2) Lwt.return_none
+  (lwt_is_perfect_val 6) (Lwt.return_some 6)
+  (lwt_is_perfect_val 7) Lwt.return_none
+  (lwt_is_perfect_val 27) Lwt.return_none
+  (lwt_is_perfect_val 28) (Lwt.return_some 28)
+  (lwt_is_perfect_val 29) Lwt.return_none
+*)
+
+
+
 (*$inject
   let pp_list_of_int l = String.concat " " (List.map string_of_int l)
 *)
@@ -34,6 +59,12 @@ let perfect_numbers n =
   loop 1
 (*$= perfect_numbers as pn & ~printer:pp_list_of_int
   (pn 100) [6; 28]
+*)
+
+(*
+let lwt_perfect_numbers n =
+  let nums = range 1 n in
+  let res = List.map lwt_is_perfect nums in
 *)
 
 let rec range ?(step=1) a b =
