840a51eb4f05fd5e067ab4188ad3b5dd5133a546
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2016-12-22 18:04:06 +0100

    + setup Makefile build

--- OpiumPlayground/hello_world.ml
+++ OpiumPlayground/hello_world.ml
@@ -1,4 +1,4 @@
-(* vim: set ft=ocaml sw=2 ts=2 *)
+(* vim: set ft=ocaml sw=2 ts=2: *)
 
 (* hello_world.ml
  * A n Opium demo, taken from
@@ -9,28 +9,28 @@ open Opium.Std
 
 type person = {
   name: string;
-age: int;
+	age: int;
 }
 
 let json_of_person { name ; age } =
-          let open Ezjsonm in
-            dict [ "name", (string name)
-                   ; "age", (int age) ]
+	let open Ezjsonm in
+	dict [ "name", (string name)
+			 ; "age", (int age) ]
 
 let print_param = put "/hello/:name" begin fun req ->
-          `String ("Hello " ^ param req "name") |> respond'
+	`String ("Hello " ^ param req "name") |> respond'
 end
 
 let print_person = get "/person/:name/:age" begin fun req ->
-          let person = {
-                      name = param req "name";
-                          age = "age" |> param req |> int_of_string;
-                            } in
-            `Json (person |> json_of_person) |> respond'
+	let person = {
+		name = param req "name";
+		age = "age" |> param req |> int_of_string;
+		} in
+	`Json (person |> json_of_person) |> respond'
 end
 
 let _ =
-          App.empty
-            |> print_param
-              |> print_person
-                |> App.run_command
+	App.empty
+	|> print_param
+	|> print_person
+	|> App.run_command
