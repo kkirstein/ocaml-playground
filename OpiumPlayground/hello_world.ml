7d1c43db5b1f0674b00ac0861af2ee10ad710f13
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2016-12-22 15:44:59 +0100

    + opium hello_world

---
+++ OpiumPlayground/hello_world.ml
@@ -0,0 +1,36 @@
+(* vim: set ft=ocaml sw=2 ts=2 *)
+
+(* hello_world.ml
+ * A n Opium demo, taken from
+ * https://github.com/rgrinberg/opium
+ *)
+
+open Opium.Std
+
+type person = {
+  name: string;
+age: int;
+}
+
+let json_of_person { name ; age } =
+          let open Ezjsonm in
+            dict [ "name", (string name)
+                   ; "age", (int age) ]
+
+let print_param = put "/hello/:name" begin fun req ->
+          `String ("Hello " ^ param req "name") |> respond'
+end
+
+let print_person = get "/person/:name/:age" begin fun req ->
+          let person = {
+                      name = param req "name";
+                          age = "age" |> param req |> int_of_string;
+                            } in
+            `Json (person |> json_of_person) |> respond'
+end
+
+let _ =
+          App.empty
+            |> print_param
+              |> print_person
+                |> App.run_command
