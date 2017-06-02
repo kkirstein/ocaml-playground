6a8052e1936065adcb12d457cede6021352cb57b
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2017-06-02 17:57:16 +0200

    ! re-indent source

--- Benchmark/bin/benchmark.ml
+++ Benchmark/bin/benchmark.ml
@@ -31,56 +31,55 @@ let rec worker_ports ?(base=5550) num =
 
 (* main entry point *)
 let bench enable_pn_worker = Lwt_main.run begin
-  let open Lwt in
-  Lwt_io.printl "Fibonacci numbers" >>= fun () ->
-  Lwt_io.printl "=================" >>= fun () ->
+    let open Lwt in
+    Lwt_io.printl "Fibonacci numbers" >>= fun () ->
+    Lwt_io.printl "=================" >>= fun () ->
 
-  let%lwt res_fib_naive = lwt_time_it Fibonacci.fib_naive 35
-  and res_fib = lwt_time_it Fibonacci.fib 35
-  and res_fib_2 = lwt_time_it Fibonacci.fib 1000
-  in
-  Lwt_io.printf "fib_naive(35) = %d (Elapsed time %.3fs)\n"
-    res_fib_naive.result res_fib_naive.elapsed >>= fun () ->
-  Lwt_io.printf "fib(35) = %s (Elapsed time %.3fs)\n"
-    (Big_int.string_of_big_int res_fib.result) res_fib.elapsed >>= fun () ->
-  Lwt_io.printf "fib(1000) = %s (Elapsed time %.3fs)\n"
-    (Big_int.string_of_big_int res_fib_2.result) res_fib_2.elapsed >>= fun () ->
-  Lwt_io.printl "" >>= fun () ->
+    let%lwt res_fib_naive = lwt_time_it Fibonacci.fib_naive 35
+    and res_fib = lwt_time_it Fibonacci.fib 35
+    and res_fib_2 = lwt_time_it Fibonacci.fib 1000
+    in
+    Lwt_io.printf "fib_naive(35) = %d (Elapsed time %.3fs)\n"
+      res_fib_naive.result res_fib_naive.elapsed >>= fun () ->
+    Lwt_io.printf "fib(35) = %s (Elapsed time %.3fs)\n"
+      (Big_int.string_of_big_int res_fib.result) res_fib.elapsed >>= fun () ->
+    Lwt_io.printf "fib(1000) = %s (Elapsed time %.3fs)\n"
+      (Big_int.string_of_big_int res_fib_2.result) res_fib_2.elapsed >>= fun () ->
+    Lwt_io.printl "" >>= fun () ->
+    Lwt_io.flush Lwt_io.stdout >>= fun () ->
 
-  Lwt_io.printl "Perfect numbers" >>= fun () ->
-  Lwt_io.printl "===============" >>= fun () ->
-  lwt_time_it Perfect_number.perfect_numbers pn_limit >>= fun res ->
-  Lwt_io.printf "perfect_numbers(%d) = %s (Elapsed time %.3fs)\n"
-    pn_limit (string_of_int_list res.result) res.elapsed >>= fun () ->
-  lwt_time_it Perfect_number.perfect_numbers_c pn_limit >>= fun res ->
-  Lwt_io.printf "perfect_numbers_c(%d) = %s (Elapsed time %.3fs)\n"
-    pn_limit (string_of_int_list res.result) res.elapsed >>= fun () ->
+    Lwt_io.printl "Perfect numbers" >>= fun () ->
+    Lwt_io.printl "===============" >>= fun () ->
+    lwt_time_it Perfect_number.perfect_numbers pn_limit >>= fun res ->
+    Lwt_io.printf "perfect_numbers(%d) = %s (Elapsed time %.3fs)\n"
+      pn_limit (string_of_int_list res.result) res.elapsed >>= fun () ->
+    lwt_time_it Perfect_number.perfect_numbers_c pn_limit >>= fun res ->
+    Lwt_io.printf "perfect_numbers_c(%d) = %s (Elapsed time %.3fs)\n"
+      pn_limit (string_of_int_list res.result) res.elapsed >>= fun () ->
 
-  if enable_pn_worker then begin
-    Lwt_io.printf "Starting worker on ports (%s) .."
-      (string_of_int_list (worker_ports num_worker)) >>= fun () ->
-    Lwt_list.map_s Perfect_number.start_worker (worker_ports num_worker) >>= fun _ ->
-    Lwt_io.printl " done." >>= fun () ->
-    lwt_time_it (Perfect_number.perfect_numbers_zmq (worker_ports num_worker)) pn_limit >>= fun res ->
-    bind res.result (fun r -> Lwt_io.printf "perfect_numbers_zmq(%d) = %s (Elapsed time %.3fs)\n"
-      pn_limit (string_of_int_list r) res.elapsed)
+    if enable_pn_worker then begin
+      Lwt_io.printf "Starting worker on ports (%s) .."
+        (string_of_int_list (worker_ports num_worker)) >>= fun () ->
+      Lwt_list.map_s Perfect_number.start_worker (worker_ports num_worker) >>= fun _ ->
+      Lwt_io.printl " done." >>= fun () ->
+      lwt_time_it (Perfect_number.perfect_numbers_zmq (worker_ports num_worker)) pn_limit >>= fun res ->
+      bind res.result (fun r -> Lwt_io.printf "perfect_numbers_zmq(%d) = %s (Elapsed time %.3fs)\n"
+                          pn_limit (string_of_int_list r) res.elapsed)
+    end
+    else return_unit >>= fun () ->
+
+      Lwt_io.printl "" >>= fun () ->
+      Lwt_io.flush Lwt_io.stdout >>= fun () ->
+
+
+      Lwt_io.printl "Mandelbrot set" >>= fun () ->
+      Lwt_io.printl "==============" >>= fun () ->
+      lwt_time_it (fun _ -> Mandelbrot.mandelbrot 640 480 (-0.5) 0.0 (4.0/.640.)) () >>= fun res ->
+      Lwt_io.printf "mandelbrot(640x480) (Elapsed time %.3fs)\n" res.elapsed >>= fun () ->
+      lwt_time_it (fun _ -> Mandelbrot.mandelbrot 1920 1200 (-0.5) 0.0 (4.0/.1200.)) () >>= fun res ->
+      Lwt_io.printf "mandelbrot(1920x1200) (Elapsed time %.3fs)\n" res.elapsed >>= fun () ->
+      return (Image.write_ppm res.result "mandelbrot_640_480.ppm")
   end
-  else return_unit >>= fun () ->
-
-  Lwt_io.printl "" >>= fun () ->
-  Lwt_io.printl "Mandelbrot set" >>= fun () ->
-  Lwt_io.printl "==============" >>= fun () ->
-  lwt_time_it (fun _ -> Mandelbrot.mandelbrot 640 480 (-0.5) 0.0 (4.0/.640.)) () >>= fun res ->
-  Lwt_io.printf "mandelbrot(640x480) (Elapsed time %.3fs)\n" res.elapsed >>= fun () ->
-  lwt_time_it (fun _ -> Mandelbrot.mandelbrot 1920 1200 (-0.5) 0.0 (4.0/.1200.)) () >>= fun res ->
-  Lwt_io.printf "mandelbrot(1920x1200) (Elapsed time %.3fs)\n" res.elapsed >>= fun () ->
-  return (Image.write_ppm res.result "mandelbrot_640_480.ppm") (* >>= fun () ->
-
-  Lwt_io.printl "" >>= fun () ->
-
-  Lwt_io.printl "Press ENTER to continue.." >>= fun () ->
-  Lwt_io.read_char Lwt_io.stdin >>= fun c -> Lwt.return_unit *)
-end
 
 
 (* cmdliner options *)
@@ -108,5 +107,5 @@ let cmd =
 (* start main *)
 let () =
   match Term.eval cmd with
-    | `Error _  -> exit 1
-    | _         -> exit 0
+  | `Error _  -> exit 1
+  | _         -> exit 0
