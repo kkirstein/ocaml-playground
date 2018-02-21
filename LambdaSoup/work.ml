8aa9a08ba725ae540388c7ddaad27ed86a5e0814
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2018-02-21 19:41:48 +0100

    + first trials for html parsing

---
+++ LambdaSoup/work.ml
@@ -0,0 +1,9 @@
+(* vim: set ft=ocaml sw=2 ts=2: *)
+
+(* uri of example document *)
+let wiki_uri = "https://en.wikipedia.org/wiki/Static_web_page"
+
+(* load local document *)
+let wiki_page = "./example_html/Static web page - Wikipedia.html"
+
+
