288d0330238ebead2ecb7aa75b7132eb59fe6441
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2020-08-30 21:05:07 +0200

    convert image to grayscale

--- Owl/owl_playground/bin/main.ml
+++ Owl/owl_playground/bin/main.ml
@@ -3,7 +3,9 @@
 
 (* a test image *)
 (* let image_path = "../../../../test_data/mandelbrot_1920_1200.png" *)
-let image_path = "./test_data/mandelbrot_1920_1200.png"
+let image_folder = "./test_data"
+
+let image_path = Filename.concat image_folder "mandelbrot_1920_1200.png"
 
 (** prints some information of given image data *)
 let print_img_info img =
@@ -25,13 +27,35 @@ let image_to_ndarray img =
   | 3 -> Ok (S.reshape img_data [| w; h; 3 |])
   | x -> Error (`Invalid_dimension x)
 
-  (** convert given image data to grayscale *)
-  let to_grayscale img =
-    let open Owl.Dense.Ndarray in
-    match S.num_dims img with
-  | 1 -> Ok (img)
-  | 3 -> Ok (failwith "not implemented")
+(** convert given image data to grayscale *)
+let to_grayscale img =
+  let open Owl.Dense.Ndarray in
+  match S.num_dims img with
+  | 1 -> Ok img
+  | 3 ->
+      let chans = S.split ~axis:2 [| 1; 1; 1 |] img in
+      let r, g, b = (chans.(0), chans.(1), chans.(2)) in
+      Ok S.((r *$ 0.2125) + (g *$ 0.7154) + (b *$ 0.0721))
   | x -> Error (`Invalid_dimension x)
+
+(** write image data to file *)
+(*
+let write ?(fmt = `PNG) file_path img =
+  let open Owl.Dense.Ndarray.S in
+  let exporter = match fmt with `PNG -> Stb_image_write.png in
+  match num_dims img with
+  | 2 ->
+      let dims = shape img in
+      let w, h, c = (dims.(0), dims.(1), 1) in
+      let img_buf = reshape img [| w * h * c |] in
+      Ok (exporter file_path ~w ~h ~c img_buf)
+  | 3 ->
+      let dims = shape img in
+      let w, h, c = (dims.(0), dims.(1), dims.(2)) in
+      let img_buf = reshape img [| w * h * c |] in
+      Ok (exporter file_path ~w ~h ~c img_buf)
+  | x -> Error (`Invalid_dimension x)
+*)
 
 (* main entry point *)
 let () =
@@ -42,5 +66,7 @@ let () =
   | Ok img_buf ->
       Printf.printf " done (%d:%d).\n" (Stb_image.width img_buf)
         (Stb_image.height img_buf);
-      let img_data = image_to_ndarray img_buf in
-      ignore (print_img_info (Result.get_ok img_data))
+      let img = image_to_ndarray img_buf |> Result.get_ok in
+      ignore (print_img_info img);
+      let gray = to_grayscale img |> Result.get_ok in
+      ignore (print_img_info gray)
