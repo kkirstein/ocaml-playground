02676532b12704995f01ec4a0ddeb9d922e46f1f
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2020-08-31 10:55:36 +0200

    save grayscale image as PNG

--- Owl/owl_playground/bin/main.ml
+++ Owl/owl_playground/bin/main.ml
@@ -16,6 +16,10 @@ let print_img_info img =
   | 2 -> Ok (Printf.printf "w:%d h:%d\n" dims.(0) dims.(1))
   | x -> Error (`Invalid_dimension x)
 
+(** Generates descriptive message for given error *)
+let error_msg = function
+  | `Invalid_dimension x -> "Invalid dimension " ^ string_of_int x
+
 (** convert given image buffer to Ndarray *)
 let image_to_ndarray img =
   let open Owl.Dense.Ndarray in
@@ -31,31 +35,43 @@ let image_to_ndarray img =
 let to_grayscale img =
   let open Owl.Dense.Ndarray in
   match S.num_dims img with
-  | 1 -> Ok img
+  | 2 -> Ok img
   | 3 ->
       let chans = S.split ~axis:2 [| 1; 1; 1 |] img in
       let r, g, b = (chans.(0), chans.(1), chans.(2)) in
       Ok S.((r *$ 0.2125) + (g *$ 0.7154) + (b *$ 0.0721))
   | x -> Error (`Invalid_dimension x)
 
-(** write image data to file *)
-(*
-let write ?(fmt = `PNG) file_path img =
-  let open Owl.Dense.Ndarray.S in
-  let exporter = match fmt with `PNG -> Stb_image_write.png in
-  match num_dims img with
-  | 2 ->
-      let dims = shape img in
-      let w, h, c = (dims.(0), dims.(1), 1) in
-      let img_buf = reshape img [| w * h * c |] in
-      Ok (exporter file_path ~w ~h ~c img_buf)
-  | 3 ->
-      let dims = shape img in
-      let w, h, c = (dims.(0), dims.(1), dims.(2)) in
-      let img_buf = reshape img [| w * h * c |] in
-      Ok (exporter file_path ~w ~h ~c img_buf)
+(** Converts an ImageBuffer to Owl's Ndarray *)
+let i2s _img = failwith "not implemented"
+
+(** Converts a Ndarray to an ImageBuffer *)
+let s2i nd =
+  let open Owl.Dense.Ndarray in
+  match S.num_dims nd with
+  | 1 | 3 ->
+      let len = S.shape nd |> Array.fold_left ( * ) 1 in
+      let buf = Bigarray.(Array1.create int8_unsigned c_layout len) in
+      S.iteri (fun i x -> buf.{i} <- int_of_float (x *. 255.0)) nd;
+      Ok buf
   | x -> Error (`Invalid_dimension x)
-*)
+
+(** write image data to file *)
+let write ?(fmt = `PNG) file_path img =
+  let open Owl.Dense.Ndarray in
+  let exporter = match fmt with `PNG -> Stb_image_write.png in
+  match S.num_dims img with
+  | 2 ->
+      let dims = S.shape img in
+      let w, h, c = (dims.(0), dims.(1), 1) in
+      let img_buf = S.reshape img [| w * h * c |] |> s2i in
+      Result.bind img_buf (fun buf -> Ok (exporter file_path ~w ~h ~c buf))
+  | 3 ->
+      let dims = S.shape img in
+      let w, h, c = (dims.(0), dims.(1), dims.(2)) in
+      let img_buf = S.reshape img [| w * h * c |] |> s2i in
+      Result.bind img_buf (fun buf -> Ok (exporter file_path ~w ~h ~c buf))
+  | x -> Error (`Invalid_dimension x)
 
 (* main entry point *)
 let () =
@@ -69,4 +85,14 @@ let () =
       let img = image_to_ndarray img_buf |> Result.get_ok in
       ignore (print_img_info img);
       let gray = to_grayscale img |> Result.get_ok in
-      ignore (print_img_info gray)
+      ignore (print_img_info gray);
+      let gray_image_path =
+        Filename.concat image_folder "mandelbrot_gray.png"
+      in
+      Printf.printf "Saving %s .." gray_image_path;
+      let () =
+        match write gray_image_path gray with
+        | Ok () -> ()
+        | Error err -> failwith (error_msg err)
+      in
+      Printf.printf " done.\n"
