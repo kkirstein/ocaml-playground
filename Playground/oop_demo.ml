2ab362fc53db76d3d87e809484c8f5552a1de241
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2021-05-10 10:37:27 +0200

    play with class bindings

--- Playground/oop_demo.ml
+++ Playground/oop_demo.ml
@@ -3,8 +3,8 @@
 (* class with local defs *)
 class my_state =
 
-let value = ref 0
-in
+  let value = ref 0
+  in
 
 object
   val mutable value1 = 13
@@ -15,3 +15,22 @@ object
   method value1 = value1
   method set_value1 v = value1 <- v
 end
+
+let () =
+  let s1 = new my_state in
+  let s2 = new my_state in 
+
+  print_endline (string_of_int s1#value);
+  print_endline (string_of_int s2#value);
+
+  print_endline (string_of_int s1#value1);
+  print_endline (string_of_int s2#value1);
+
+  s1#set_value 42;
+  print_endline (string_of_int s2#value);
+
+  s1#set_value1 3;
+  print_endline (string_of_int s1#value1);
+  print_endline (string_of_int s2#value1);
+
+  ()
